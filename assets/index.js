6b7ce4657faba213b322ddb039d7661a70c29234
Baturina Dasha <shdonly@gmail.com>
2021-12-08 14:30:17 +0300

    add animation

--- assets/index.js
+++ assets/index.js
@@ -11,8 +11,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     rightButton.addEventListener('click',() => {
         if (rightButton.classList.contains("switcher__button-rightActive")) {
-            girlScreen.style.display = `none`;
-            textScreen.style.display = `block`;
+            textScreen.style.opacity = `1`;
             rightButton.classList.remove("switcher__button-rightActive");
             rightButton.classList.add("switcher__button-rightInactive");
             leftButton.classList.remove("switcher__button-leftInactive");
@@ -22,8 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     leftButton.addEventListener('click',() => {
         if (leftButton.classList.contains("switcher__button-leftActive")) {
-            girlScreen.style.display = `block`;
-            textScreen.style.display = `none`;
+            textScreen.style.opacity = `0`;
             leftButton.classList.remove("switcher__button-leftActive");
             leftButton.classList.add("switcher__button-leftInactive");
             rightButton.classList.remove("switcher__button-rightInactive");
